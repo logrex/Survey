8106668fa4ed4fb3d776f0d0a1d36f6489d0cb46
logrex <arth.sharma2010@gmail.com>
2016-09-28 12:55:17 +0530

    Custom carousel

--- webs/jet.js
+++ webs/jet.js
@@ -1,5 +1,40 @@
- $(document).ready(function(){
+ /* $(document).ready(function(){
       $('.slider').slider({full_width: false});
 	  $.varx = $( window ).height();
     $('.slider').slider({height:$.varx-50});
-	});
+	}); */
+
+	
+	function slide(Direction){
+		
+		var i=0;
+		var ele_all = document.getElementsByClassName("hideable");
+		var ele_vis_pos=-1;
+		var make_vis=-1;
+		for(i;ele_all.length;i++){
+			if(ele_all[i].style.display=="block"){
+				ele_vis_pos=i;
+				ele_all[i].style.display="none";
+				break;
+			}
+		}
+		
+		 if(!Direction==0){
+			if(ele_vis_pos==(ele_all.length-1)){
+				make_vis=0;
+			}else{
+				make_vis=ele_vis_pos+1;
+			}
+		}else{
+			if(ele_vis_pos==0){
+				make_vis=ele_all.length-1;
+			}else{
+				make_vis=ele_vis_pos-1;
+			}
+		}
+		
+		ele_all[make_vis].style.display="block";
+		 
+	}
+	
+	
