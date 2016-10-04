e5822c437deff191fdfd1ef9249f24cc3e9e2e29
logrex <arth.sharma2010@gmail.com>
2016-10-05 00:21:13 +0530

    Div Rotation

--- Super_Survey/customjs.js
+++ Super_Survey/customjs.js
@@ -4,16 +4,7 @@ function onLoad(){
 	for(var i=0;i<_parent_div.length;i++){
 		_parent_div[i].style.height=_height+"px";
 		}
-	/* 
-	var canvas=document.getElementById("myCan");
-	var brush= canvas.getContext('2d');
-	brush.strokeStyle='red';
-	brush.lineWidth=3;
-	brush.beginPath();
-	brush.moveTo(300,300);
-	brush.lineTo(300,200);
-	brush.stroke(); */
-alert(_height);
+
 }
 
 function play(){
