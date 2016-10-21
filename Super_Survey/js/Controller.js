5494d3daa8f107c9880f5dabcb9b3188b2089707
logrex <arth.sharma2010@gmail.com>
2016-10-21 23:42:21 +0530

    Circular motion Functional

--- Super_Survey/js/Controller.js
+++ Super_Survey/js/Controller.js
@@ -13,14 +13,25 @@
  
   
   var mainCtrl=function($scope){
-   
+	  
+	 var incomingQuest={'data':[{'num':'1','title':'Question is itself a question what to ask then?'},{'num':'2','title':'Wow again a question?'},{'num':'3','title':'Now question\'s has it\'s own questions?'},{'num':'4','title':'Question can be a answers?'},{'num':'5','title':'Why question is a question not a pizza?'},{'num':'6','title':'So what is the question?'}]};
+	 $scope.questions=incomingQuest.data;
+	  $scope.questLength=incomingQuest.data.length;
+	  console.log(incomingQuest.data.length+" "+$scope.questions);
+	   console.log("I am Controller"+$scope.questLength);
+	  $scope.motion="animate-origin"
+	   
+	  $scope.show='show';
+	  var i=0;
+	 
+
+	   var testin=function(){console.log("Swatch");};
+
+   $scope.testing=testin;
+
   };
    app.controller('mainCtrl',["$scope",mainCtrl]);
 
   
-  var myQuestions=function($scope){
-	  $scope.quest={'data':[{'number':'1','title':'Question is itself a question what to ask then?'},{'number':'2','title':'Question is itself a question what to ask then?'},{'number':'3','title':'Question is itself a question what to ask then?'},{'number':'4','title':'Question is itself a question what to ask then?'},{'number':'5','title':'Question is itself a question what to ask then?'},{'number':'6','title':'Question is itself a question what to ask then?'}]};
-  };
-   app.controller('myQuestions',["$scope",myQuestions]);
  
 }());
